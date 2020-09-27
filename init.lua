04018c504d155fa0faf9618ae8a8d645e0167a49
Maksim <MoNTE48@mail.ua>
2020-09-27 19:14:49 +0200

    Remove unused entities on settings change (#8)
    Also add Minetest 5.3 support for 10/11 maximal breath.

--- init.lua
+++ init.lua
@@ -3,25 +3,26 @@
 -- Copyright © 2014-2020 4aiman, Hugo Locurcio and contributors - MIT License
 -- See `LICENSE.md` included in the source distribution for details.
 
-if minetest.settings:get_bool("health_bars") == false or
-		not minetest.settings:get_bool("enable_damage")
-then return end
+local enabled = minetest.settings:get_bool("health_bars") ~= false
+if enabled then
+	enabled = minetest.settings:get_bool("enable_damage")
+end
 
--- Localize the vector distance function for better performance,
+-- Localize this functions for better performance,
 -- as it's called on every step
 local vector_distance = vector.distance
+local min = math.min
+local max_breath = minetest.PLAYER_MAX_BREATH_DEFAULT or 11
+
+local mt_5 = minetest.features.object_independent_selectionbox
 
 local function add_gauge(player)
 	if player and player:is_player() then
 		local entity = minetest.add_entity(player:get_pos(), "gauges:hp_bar")
-		local height = 19
 
-		-- Check for Minetest 0.4.17 and adjust the entity height if needed
-		-- (The entity height offset was changed in Minetest 5.0.0.)
-		local version = tonumber(minetest.get_version().string:sub(1, 1))
-		if version and version < 5 then
-			height = 9
-		end
+		-- Check Minetest version and set required entity heigh
+		-- (The entity height offset was changed in Minetest 5.0.0)
+		local height = mt_5 and 19 or 9
 
 		entity:set_attach(player, "", {x=0, y=height, z=0}, {x=0, y=0, z=0})
 		entity:get_luaentity().wielder = player
@@ -39,7 +40,8 @@ minetest.register_entity("gauges:hp_bar", {
 		local player = self.wielder
 		local gauge = self.object
 
-		if not player or not player:is_player() then
+		if not enabled or
+				not player or not player:is_player() then
 			gauge:remove()
 			return
 		elseif vector_distance(player:get_pos(), gauge:get_pos()) > 3 then
@@ -48,15 +50,23 @@ minetest.register_entity("gauges:hp_bar", {
 			return
 		end
 
-		local hp = player:get_hp() <= 20 and player:get_hp() or 20
-		local breath = player:get_breath() <= 10 and player:get_breath() or 11
+		local hp = min(player:get_hp(), 20)
+		local breath = min(player:get_breath(), max_breath)
 
 		if self.hp ~= hp or self.breath ~= breath then
+			local health_t = "health_"..hp..".png"
+			local breath_t = "breath_"..breath..".png"
+
+			if hp == 0 then
+				health_t = "blank.png"
+			end
+
+			if breath == max_breath then
+				breath_t = "blank.png"
+			end
+
 			gauge:set_properties({
-				textures = {
-					"health_"..hp..".png^"..
-					"breath_"..breath..".png"
-				}
+				textures = {health_t.."^"..breath_t}
 			})
 			self.hp = hp
 			self.breath = breath
@@ -64,6 +74,8 @@ minetest.register_entity("gauges:hp_bar", {
 	end
 })
 
-minetest.register_on_joinplayer(function(player)
-	minetest.after(1, add_gauge, player)
-end)
+if enabled then
+	minetest.register_on_joinplayer(function(player)
+		minetest.after(1, add_gauge, player)
+	end)
+end
